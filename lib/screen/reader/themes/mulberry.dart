e3a4864ea31608e3c4a9e9ff7ba4e491fd635aa6
laoboli <1293528695@qq.com>
2024-09-28 20:01:06 +0800

    refactor: color.

--- lib/screen/reader/themes/mulberry.dart
+++ lib/screen/reader/themes/mulberry.dart
@@ -6,7 +6,7 @@ final mulberryTheme = ThemeData(
         primary: Color(0xFFF75C2F),
         primaryContainer: Color(0xFFF98C6D),
         secondary: Color(0xFFA1A1A1),
-        background: Color(0xFFFAFAFA),
+        background: Color(0xFFF4F4EF),
         onBackground: Color(0xFF242424),
         surface: Color(0xFFFFFFFF),
         outline: Color(0xFFEEEEEE)));
